22f98fff577292a0e6d6a6cf9605742fa255cf3d
ayushraj049 <kashyapyuvraj049@gmail.com>
2025-11-07 09:50:27 +0530

    Add files via upload

---
+++ javaScript qasim sir/first.js
@@ -0,0 +1,1 @@
+console.log("enternal js")
---
+++ javaScript qasim sir/function.js
@@ -0,0 +1,40 @@
+//function calling definme
+// function Demo(){
+//     console.log("inside the function")
+// }
+// Demo()
+//=====================================
+//write a program to add two uo using function
+//write a program to take one integer input from the user and chek no is even or odd
+//write a program to print reverse table of n no using function
+
+// let a=10;
+// let b=20;
+// function Add(x,y){
+//     return a+b
+
+// }
+
+// let res=Add(a,b)
+// console.log(res)
+//=====================================
+// let c=parseInt(prompt("enetr input"))
+// function Chek(d){
+//     if(d%2==0){
+//         console.log("even")
+//     }
+//     else {
+//         console.log("odd")
+// }
+// }
+// Chek(c)
+//=========================================
+let c=parseInt(prompt("enetr input"))
+function Table(d){
+    for(let i=10;i>=1;i--){
+        console.log(d*i)
+    }
+}
+Table(c)
+
+
